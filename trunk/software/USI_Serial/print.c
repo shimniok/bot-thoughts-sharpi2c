1862457f42edeac89e94e13869bdd7107d9895ad
shimniok@gmail.com <shimniok@gmail.com@9cb5c4dc-5ff2-f84c-3a28-87f545295924>
2013-08-21 05:00:19 +0000

    Initial revision of USI Serial library

---
+++ trunk/software/USI_Serial/print.c
@@ -0,0 +1,55 @@
+/*
+ * print.c
+ *
+ *  Created on: Jun 12, 2013
+ *      Author: mes
+ */
+#include "usi_uart.h"
+
+// TODO: fix bug where last character isn't printed -- may be related to printUL called after print
+
+void print_init()
+{
+	USI_UART_Initialise_Transmitter();
+}
+
+
+void print(const char *s)
+{
+	if (s) {
+		while (*s) {
+			USI_UART_Transmit_Byte(*s);
+			s++;
+		}
+	}
+}
+
+
+// from Arduino source
+void printUL(unsigned long n)
+{
+    char buf[8 * sizeof(long) + 1]; // Assumes 8-bit chars plus zero byte.
+    char *str = &buf[sizeof(buf) - 1];
+
+    *str = '\0';
+
+    do {
+        unsigned long m = n;
+        n /= 10;
+        char c = m - 10 * n;
+        *--str = c + '0';
+    } while(n);
+
+    print(str);
+}
+
+// from Arduino source
+void printL(long n)
+{
+    if (n < 0) {
+        USI_UART_Transmit_Byte('-');
+        n = -n;
+    }
+    printUL(n);
+}
+
